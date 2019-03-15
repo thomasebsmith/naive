b4575dca61b9cc752d98fcb6f73e979c4090f326
Thomas Smith <thsm@umich.edu>
2019-03-14 22:31:07 -0400

    Fix bugs and add documentation in rendererMessaging.js

--- utils/rendererMessaging.js
+++ utils/rendererMessaging.js
@@ -1,19 +1,33 @@
+// utils/rendererMessaging.js
+//
+// This file provides utilities for sending messages to the /main.js background
+//  process.
+
+// Imports
 const {ipcRenderer} = require("electron");
 let messageID = 0;
 
-exports.sendMessageToMain = (msg, callback) => {
+// sendMessageToMain(msg) - Sends msg to /main.js.
+exports.sendMessageToMain = (msg) => {
   ipcRenderer.send("message", msg);
 };
 
+// sendAsyncMessageToMain(msg, callback) - Sends msg to /main.js, waiting
+//  for a response. After the response is obtained, callback is called
+//  with the replying msg from /main.js.
 exports.sendAsyncMessageToMain = (msg, callback) => {
   const replyID = messageID;
   ipcRenderer.send("async-message", {
     id: messageID,
     msg: msg
   });
-  ipcRenderer.on("reply", (event, data) => {
+  ++messageID;
+  const listener = (event, data) => {
     if (data.id === replyID) {
       callback(data.msg);
+      ipcRenderer.removeListener("reply", listener);
     }
-  });
+  };
+
+  ipcRenderer.on("reply", listener);
 };
