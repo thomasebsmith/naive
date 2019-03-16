72f9edc20cec1eaa6476310d2a18737baa7e54ff
Thomas Smith <thsm@umich.edu>
2019-03-15 23:51:51 -0400

    Add inline documentation to isProduction.js

--- utils/isProduction.js
+++ utils/isProduction.js
@@ -1,1 +1,8 @@
+// utils/isProduction.js
+//
+// This file provides a utility to check if the current app run should be
+//  in production or developer mode. Its only export is a boolean that is
+//  true iff it is production. This is determined by the presence of an
+//  external environment variable "ELECTRON_IS_DEVELOPER" which should be 1
+//  to avoid a production state.
 module.exports = require("process").env.ELECTRON_IS_DEVELOPER !== "1";
