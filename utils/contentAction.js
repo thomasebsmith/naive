1ac2874274783f8280ab16b42e527118e74e37c0
Thomas Smith <thsm@umich.edu>
2019-03-16 23:42:30 -0400

    Add inline documentation to contentAction.js

--- utils/contentAction.js
+++ utils/contentAction.js
@@ -1,3 +1,14 @@
+// utils/contentAction.js
+//
+// This file provides a function that performs an editor action ("content
+//  action"). The syntax of the function varies depending on the context in
+//  which this module is imported. If the module is imported in an HTML page,
+//  the syntax is contentAction(action, ...args). If the module is imported
+//  elsewhere (i.e. in /main.js), the syntax is contentAction(win, action,
+//  ...args).
+
+// First, detect whether the current context is HTML and whether it is the
+//  editor page itself.
 let isHTML = true;
 try {
   window && window.document;
@@ -10,6 +21,7 @@ if (isHTML && window.isEditor) {
   isEditor = true;
 }
 
+// If it is the editor page, use the actions object directly.
 let contentAction;
 if (isHTML && isEditor) {
   let win = editorWindow.requireProps;
@@ -23,11 +35,18 @@ if (isHTML && isEditor) {
     }
   };
 }
+
+// If it is another HTML page (i.e. views/main.html), post to contentEl's
+//  contentWindow (i.e. the views/editor.html window).
 else if (isHTML) {
   contentAction = (action, ...args) => {
     contentEl.contentWindow.postMessage({ action, args }, location.origin);
   };
 }
+
+// Otherwise, in a non-HTML environment, send the action to an appropriate
+//  window which can then send it to the views/editor.html page within that
+//  window.
 else {
   contentAction = (win, action, ...args) => {
     sendWindowMessage(win, {
