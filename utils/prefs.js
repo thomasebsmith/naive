541d9a9978975d31f1cf5652c0b30df729af0e22
Thomas Smith <thsm@umich.edu>
2019-03-17 23:52:42 -0400

    Add inline documentation to utils/prefs.js

--- utils/prefs.js
+++ utils/prefs.js
@@ -1,11 +1,22 @@
+// prefs.js
+//
+// This file provides utilities for getting the current and default user
+//  preferences.
+
+// Imports
 const electron = require("electron");
 const path = require("path");
 const fs = require("fs");
 
+// resetFile(filePath, defaultValues) - Writes the JSON stringified version
+//  of defaultValues to the file at filePath.
 const resetFile = (filePath, defaultValues) => {
   fs.writeFileSync(filePath, JSON.stringify(defaultValues));
 };
 
+// getFileData(filePath, defaultValues) - If the file at filePath exists,
+//  returns that files content parsed as JSON. If not, or if there is an error
+//  parsing JSON, writes defaultValues to the file and returns defaultValues.
 const getFileData = (filePath, defaultValues) => {
   try {
     return JSON.parse(fs.readFileSync(filePath));
@@ -16,29 +27,41 @@ const getFileData = (filePath, defaultValues) => {
   }
 };
 
+// UserDataFile - Represents a preferences or other user data file.
 class UserDataFile {
+  // constructor(fileName, defaultValues) - Creates a UserDataFile that will
+  //  read from and write to fileName (relative to the electron userData
+  //  directory). The default values are specified in defaultValues.
   constructor(fileName, defaultValues) {
     const userDataPath = (electron.app ||
-electron.remote.app).getPath("userData");
+      electron.remote.app).getPath("userData");
     this.defaultValues = defaultValues;
     this.path = path.join(userDataPath, fileName + ".json");
     this.data = getFileData(this.path, this.defaultValues);
   }
+
+  // get(key) - Returns the user data entry with name "key".
   get(key) {
     if (this.data[key] === undefined) {
       return;
     }
     return JSON.parse(JSON.stringify(this.data[key]));
   }
+
+  // set(key, value) - Sets the user data entry with name "key" to "value".
+  //  Updates the preferences file accordingly.
   set(key, value) {
     this.data[key] = value;
     fs.writeFileSync(this.path, JSON.stringify(this.data));
   }
+
+  // reset() - Resets the user data file to its default.
   reset() {
     resetFile(this.path, this.defaultValues);
   }
 }
 
+// Constants for Naive prefs.
 const prefsFileName = "prefs";
 const defaultPrefs = {
   style: {
@@ -92,6 +115,7 @@ const defaultPrefs = {
   }
 };
 
+// Prefs - Represents the Naive preferences user data file.
 class Prefs extends UserDataFile {
   constructor() {
     super(prefsFileName, defaultPrefs);
