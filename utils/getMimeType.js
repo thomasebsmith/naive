43f605dc69ed5678b14c8c2ee797d0e525a3292e
Thomas Smith <thsm@umich.edu>
2019-03-18 23:53:45 -0400

    Add more extensions and documentation to getMimeType.js

--- utils/getMimeType.js
+++ utils/getMimeType.js
@@ -1,7 +1,15 @@
+// utils/getMimeType.js
+//
+// This file provides a function for getting the mime type for a given file
+//  extension. Currently, only a few basic file extensions are supported. The
+//  text/plain mime type is returned for unsupported file extensions.
 const defaultMimeType = "text/plain";
 
 const mimeTypes = {
+  "css": "text/css",
   "js": "application/javascript",
+  "htm": "text/html",
+  "html": "text/html",
   "txt": "text/plain"
 };
 
