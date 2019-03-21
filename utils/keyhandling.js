6c911e33dc89fc3a09d72788fd891eb3c7f21909
Thomas Smith <thsm@umich.edu>
2019-03-20 23:51:17 -0400

    Add inline documentation for key handling

--- utils/keyhandling.js
+++ utils/keyhandling.js
@@ -1,22 +1,32 @@
+// utils/keyhandling.js
+//
+// Provides a function for responding to key events. This function, given an
+//  event target, listens for keyup events on that target. When they occur,
+//  it dispatches an appropriate editor action via the contentAction function
+//  provided as an argument.
 exports.handleKeys = (target, contentAction) => {
   target.addEventListener("keyup", (event) => {
     switch (event.key) {
+      // Cursor movement
       case "ArrowLeft":
         contentAction("cursorLeft");
         break;
       case "ArrowRight":
         contentAction("cursorRight");
         break;
+      // Deleting
       case "Backspace":
       case "Delete":
         contentAction("removeBeforeCursor", 1);
         break;
+      // Special whitespace characters
       case "Enter":
         contentAction("insertAtCursor", "\n");
         break;
       case "Tab":
         contentAction("insertAtCursor", "\t");
         break;
+      // Normal characters
       default:
         if (event.key.length === 1) {
           contentAction("insertAtCursor", event.key);
