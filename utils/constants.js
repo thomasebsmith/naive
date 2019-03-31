c6ed53f073f9154bcac68287c764e8fe9f794653
Thomas Smith <thsm@umich.edu>
2019-03-30 23:32:23 -0400

    Add inline documentation to utils/constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,9 @@
+// utils/constants.js
+//
+// This file contains application-wide constants that can be used in both the
+// background and renderer processes. There are constants for the app name,
+// the message sent when content is loaded, the message sent for messages that
+// should receive a reply, and a noop function.
 module.exports = {
   appName: "Naive",
   contentLoaded: "contentLoaded",
