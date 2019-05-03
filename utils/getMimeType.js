eedde73abd4e8761d1967b6850d0b01594ed2da8
Thomas Smith <thsm@umich.edu>
2019-05-02 23:10:01 -0400

    Add larger list of file extensions to MIME types

--- utils/getMimeType.js
+++ utils/getMimeType.js
@@ -6,16 +6,39 @@
 const defaultMimeType = "text/plain";
 
 const mimeTypes = {
+  "asm": "text/x-assembly-source",
   "c": "text/x-c-source",
   "cpp": "text/x-cplusplus-source",
+  "cs": "text/x-csharp-source",
   "css": "text/css",
+  "csv": "text/csv",
+  "erl": "text/x-erlang-source",
+  "go": "text/x-go-source",
   "h": "text/x-c-source",
   "hpp": "text/x-cplusplus-source",
+  "hs": "text/x-haskell-source",
   "htm": "text/html",
   "html": "text/html",
+  "java": "text/x-java-source",
   "js": "application/javascript",
+  "mat": "application/x-matlab",
   "md": "text/markdown",
-  "txt": "text/plain"
+  "mjs": "application/javascript",
+  "php": "application/x-php",
+  "pl": "application/x-perl",
+  "py": "application/x-python",
+  "r": "application/x-r",
+  "rb": "application/x-ruby",
+  "rs": "text/x-rust-source",
+  "s": "text/x-assembly-source",
+  "scala": "text/x-scala-source",
+  "sh": "application/x-sh",
+  "svg": "image/svg+xml",
+  "swift": "text/x-swift-source",
+  "txt": "text/plain",
+  "vb": "text/x-visualbasic-source",
+  "xhtml": "application/xhtml+xml",
+  "xml": "text/xml"
 };
 
 const getMimeType = (ext) => {
