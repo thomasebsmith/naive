bea8ae3926ed3a40a9c0812d56da0055cbed2e6d
Thomas Smith <thsm@umich.edu>
2019-06-04 23:47:09 -0400

    Add method to generate CSS variables from Styles

--- utils/theme.js
+++ utils/theme.js
@@ -1,9 +1,20 @@
 class Style {
-  constructor(color, weight, style, decoration) {
+  constructor(name, color, weight, style, decoration) {
+    this.name = name;
     this.color = color;
     this.weight = weight;
     this.style = style;
     this.decoration = decoration;
+  }
+  addToDocument(document) {
+    document.body.style.setProperty("--user-" + this.name + "-color",
+      this.color);
+    document.body.style.setProperty("--user-" + this.name + "-weight",
+      this.weight);
+    document.body.style.setProperty("--user-" + this.name + "-style",
+      this.style);
+    document.body.style.setProperty("--user-" + this.name + "-decoration",
+      this.decoration);
   }
 };
 
